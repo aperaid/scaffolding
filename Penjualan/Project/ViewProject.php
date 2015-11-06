55a073d834abe8f0a2f146638c2baa2b057a4143
Victor Hartanto <hartantovictor27@gmail.com>
2015-11-06 13:57:01 +0700

    View Project

--- Penjualan/Project/ViewProject.php
+++ Penjualan/Project/ViewProject.php
@@ -36,7 +36,7 @@ if (isset($_GET['PCode'])) {
   $colname_View = $_GET['PCode'];
 }
 mysql_select_db($database_Connection, $Connection);
-$query_View = sprintf("SELECT project.*, customer.Alamat, customer.Company, customer.CompPhone, customer.CCode FROM project INNER JOIN customer ON project.CCode=customer.CCode WHERE PCode = %s", GetSQLValueString($colname_View, "text"));
+$query_View = sprintf("SELECT project.*, customer.Alamat, customer.Company, customer.CompPhone, customer.Customer, customer.CustPhone, customer.CCode FROM project INNER JOIN customer ON project.CCode=customer.CCode WHERE PCode = %s", GetSQLValueString($colname_View, "text"));
 $View = mysql_query($query_View, $Connection) or die(mysql_error());
 $row_View = mysql_fetch_assoc($View);
 $totalRows_View = mysql_num_rows($View);
@@ -47,128 +47,292 @@ $Menu = mysql_query($query_Menu, $Connection) or die(mysql_error());
 $row_Menu = mysql_fetch_assoc($Menu);
 $totalRows_Menu = mysql_num_rows($Menu);
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-<link href="../../Button.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-body {
-	background-image: url(../../Image/Wood.png);
-	background-repeat: no-repeat;
-}
-</style>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>BDN ERP | View Project</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.5 -->
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../../plugins/datatables/dataTables.bootstrap.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
 
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
+<body class="hold-transition skin-blue fixed sidebar-mini">
+<div class="wrapper">
 
-<body>
-<div style="float:left;width:15%">
-  <table width="200" border="0">
-    <tbody>
-      <tr>
-        <th>&nbsp;</th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-      </tr>
-      
-				<?php do { ?>    
-                	<tr>
-                    <td class="Menu">
-                    <a href="../<?php echo $row_Menu['link']; ?>">
-                    <button type="button" class="button">
-					<?php echo $row_Menu['nama']; ?></button></a></td>
-                    </tr>
-                    
-                <?php } while ($row_Menu = mysql_fetch_assoc($Menu)); ?>
-                    <tr>
-                    <td class="Menu">&nbsp;</td>
-                    </tr>
-                    
-    </tbody>
-  </table>
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>BDN</b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg">PT. <b>BDN</b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Buka/Tutup</span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <span class="hidden-xs">Administrator</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  Admin - Adminstrator
+                  <small>Super Profile</small>
+                </p>
+              </li>
+              
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+  </header>
+  
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MENU</li>
+        <?php do { ?>
+        <li><a href="../<?php echo $row_Menu['link']; ?>"><i class="<?php echo $row_Menu['icon']; ?>"></i> <span><?php echo $row_Menu['nama']; ?></span></a></li>
+        <?php } while ($row_Menu = mysql_fetch_assoc($Menu)); ?>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+  
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        View Project
+        <small>View</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Project</a></li>
+        <li class="active">View Project</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <!-- Horizontal Form -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Company Detail</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form id="form1" name="form1" method="post" class="form-horizontal">
+              <div class="box-body with-border">
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Project Code</label>
+                  <div class="col-sm-6">
+                    <input id="PCode" name="PCode" type="text" class="form-control" value="<?php echo $row_View['PCode']; ?>"  readonly>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Project Name</label>
+                  <div class="col-sm-6">
+                    <input id="Project" name="Project" type="text" class="form-control" value="<?php echo $row_View['Project']; ?>"  readonly>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Alamat</label>
+                  <div class="col-sm-6">
+                    <input id="Alamat" name="Alamat" type="text" class="form-control" value="<?php echo $row_View['Alamat']; ?>"  readonly>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Company Code</label>
+                  <div class="col-sm-6">
+                    <input id="CCode" name="Ccode" type="text" class="form-control" value="<?php echo $row_View['CCode']; ?>"  readonly>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Company Name</label>
+                  <div class="col-sm-6">
+                    <input id="Company" name="Company" type="text" class="form-control" value="<?php echo $row_View['Company']; ?>"  readonly>
+                  </div>
+                  <label class="col-sm-1 control-label">Telp</label>
+                  <div class="col-sm-3">
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-phone"></i>
+                      </div>
+                      <input id="CompPhone" name="CompPhone" type="text" class="form-control" value="<?php echo $row_View['CompPhone']; ?>"  readonly>
+                    </div>
+                  </div>
+                </div>
+                <hr>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">CP</label>
+                  <div class="col-sm-6">
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-user"></i>
+                      </div>
+                      <input id="Customer" name="Customer" type="text" class="form-control" value="<?php echo $row_View['Customer']; ?>"  readonly>
+                    </div>
+                  </div>
+                  <label class="col-sm-1 control-label">Telp</label>
+                  <div class="col-sm-3">
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-phone"></i>
+                      </div>
+                      <input id="CustPhone" name="CustPhone" type="text" class="form-control" value="<?php echo $row_View['CustPhone']; ?>"  readonly>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <a href="EditProject.php?PCode=<?php echo $row_View['PCode']; ?>"><button type="button" class="btn btn-info pull-right">Edit</button></a>
+                <div class="btn-group"><a href="Project.php"><button type="button" class="btn btn-default pull-left">Back</button></a></div>
+                <div class="btn-group" ><a href="DeleteProject.php?Id=<?php echo $row_View['Id']; ?>" onclick="return confirm('Delete Project?')"><button type="button" class="btn btn-danger pull-left">Delete</button></a></div>
+              </div>
+              <input type="hidden" name="MM_update" value="form1">
+              <!-- /.box-footer -->
+            </form>
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  <!-- Footer Wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> `1.0.0
+    </div>
+    <strong>Copyright &copy; 2015 <a href="http://apera.id">Apera Indonesia</a>.</strong> All rights
+    reserved.
+  </footer>
+  <!-- /.footer-wrapper -->
+  
+  <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
 </div>
 
-<div style="float:left;width:85%">
+<!-- jQuery 2.1.4 -->
+<script src="../../	plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<!-- Bootstrap 3.3.5 -->
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false
+    });
+  });
+</script>
+<script>
+function capital() {
+    var x = document.getElementById("CCode");
+    x.value = x.value.toUpperCase();
+	var x = document.getElementById("Company");
+    x.value = x.value.toUpperCase();
+}
+</script>
+<script>
+  $(document).ready(function() {
+    $("#dialog").dialog({
+      autoOpen: false,
+      modal: true
+    });
+  });
 
-<table width="950" border="0">
+  $(".confirmLink").click(function(e) {
+    e.preventDefault();
+    var targetUrl = $(this).attr("href");
 
+    $("#dialog").dialog({
+      buttons : {
+        "Confirm" : function() {
+          window.location.href = targetUrl;
+        },
+        "Cancel" : function() {
+          $(this).dialog("close");
+        }
+      }
+    });
 
-  <tbody>
-    <tr>
-      <th align="center"><h2><?php echo $row_View['Project']; ?></h2></th>
-    </tr>
-  </tbody>
-</table>
-</p>
-<form id="form1" name="form1" method="POST">
-  <table width="1005" border="0">
-    <tbody>
-      <tr>
-        <th width="75">&nbsp;</th>
-        <th width="125" align="right">Project Code</th>
-        <th width="50" align="right">&nbsp;</th>
-        <th colspan="4" align="left"><input name="PCode" type="text" class="textview" id="PCode" style="width:512px;" value="<?php echo $row_View['PCode']; ?>" readonly></th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-        <th align="right">Project</th>
-        <th align="right">&nbsp;</th>
-        <th colspan="4" align="left"><input name="Project" type="text" class="textview" id="Project" style="width:512px;" value="<?php echo $row_View['Project']; ?>" readonly></th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-        <th align="right">Alamat</th>
-        <th align="right">&nbsp;</th>
-        <th colspan="4" align="left"><input name="Alamat" type="text" class="textview" id="Alamat" style="width:512px;" value="<?php echo $row_View['Alamat']; ?>" readonly></th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-        <th align="right">Company</th>
-        <th align="right">&nbsp;</th>
-        <th width="150" align="left"><input name="Company" type="text" class="textview" id="Company" value="<?php echo $row_View['Company']; ?>" readonly></th>
-        <th width="100" align="right">Phone</th>
-        <th width="50" align="right">&nbsp;</th>
-        <td width="425"><input name="CompPhone" type="text" class="textview" id="CompPhone" value="<?php echo $row_View['CompPhone']; ?>" readonly></td>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-        <th align="right">Customer Code</th>
-        <th align="right">&nbsp;</th>
-        <th colspan="4" align="left"><input name="CCode" type="text" class="textview" id="CCode" style="width:512px" value="<?php echo $row_View['CCode']; ?>" readonly></th>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td align="center"><a href="EditProject.php?PCode=<?php echo $row_View['PCode']; ?>">
-          <button type="button" class="button2">Edit Project</button>
-        </a></td>
-        <td colspan="2" align="left"><a><button type="button" class="button2">Print</button></a></td>
-        <td align="left"><a href="Project.php"><button type="button" class="button2">Back</button></a></td>
-        <td>&nbsp;</td>
-      </tr>
-    </tbody>
-  </table>
-</form>
+    $("#dialog").dialog("open");
+  });
+</script>
 </body>
 </html>
 <?php
+mysql_free_result($Menu);
 mysql_free_result($View);
 ?>
