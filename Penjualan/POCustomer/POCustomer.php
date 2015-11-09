d3cf562a24ed9eaab75c7293b25d8261deb277f0
Victor Hartanto <hartantovictor27@gmail.com>
2015-11-09 13:12:42 +0700

    PO half

--- Penjualan/POCustomer/POCustomer.php
+++ Penjualan/POCustomer/POCustomer.php
@@ -74,121 +74,210 @@ $Menu = mysql_query($query_Menu, $Connection) or die(mysql_error());
 $row_Menu = mysql_fetch_assoc($Menu);
 $totalRows_Menu = mysql_num_rows($Menu);
 ?>
-<!------------------------------------------------------->
-<link rel="stylesheet" type="text/css" href="../../JQuery/Layout/layout.css">
-<script type="text/javascript" src="../../JQuery/Layout/jquery.js"></script>
-<script type="text/javascript" src="../../JQuery/Layout/jquery.ui.all.js"></script>
-<script type="text/javascript" src="../../JQuery/Layout/jquery.layout.js"></script>
-<script type='text/javascript'>
-	var jq126 = jQuery.noConflict();
-</script>
-<script type="text/javascript">
-	var myLayout;// a var is required because this page utilizes: myLayout.allowOverflow() method
-	
-	jq126(document).ready(function () {
-	myLayout = jq126('body').layout({
-		// enable showOverflow on west-pane so popups will overlap north pane
-		west__showOverflowOnHover: true
-	
-	//,	west__fxSettings_open: { easing: "easeOutBounce", duration: 750 }
-	});
-	
-	});
-	
-</script>
-<!------------------------------------------------------->
-<!-- DataTables CSS -->
-<link rel="stylesheet" type="text/css" href="../../JQuery/DataTable/css/jquery.dataTables.css">
-<!-- jQuery -->
-<script type="text/javascript" charset="utf8" src="../../JQuery/DataTable/js/jquery.js"></script>
-<!-- DataTables -->
-<script type="text/javascript" charset="utf8" src="../../JQuery/datatable/js/jquery.dataTables.js"></script>
-<script>
-	$(document).ready( function () {
-	    $('#contentTable').DataTable();
-	} );
-	
-</script>
-<!------------------------------------------------------->
+
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>PT. BDN | PO Customer</title>
-		<link href="../../Button.css" rel="stylesheet" type="text/css">
-	</head>
-	<body>
-		<div class="ui-layout-north">
-			<div class="title">
-				PO CUSTOMER
-			</div>
-		</div>
-		<div class="ui-layout-west">
-			<table class="menuTable">
-					
-				<?php do { ?>    
-                	<tr>
-                    <td class="Menu">
-                    <a href="../<?php echo $row_Menu['link']; ?>">
-                    <button type="button" class="button">
-					<?php echo $row_Menu['nama']; ?></button></a></td>
-                    </tr>
-                    
-                <?php } while ($row_Menu = mysql_fetch_assoc($Menu)); ?>
-                    <tr>
-                    <td class="Menu">&nbsp;</td>
-                    </tr>
-                    
-			</table>
-		</div>
-		<div class="ui-layout-south">
-			<div class="footer">
-				PT. Berlian Djaya Nusantara
-			</div>
-			<div class="copyright">
-				Powered by Apera ERP
-			</div>
-		</div>
-		<div class="ui-layout-east">
-			<table class="menuTable">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>BDN ERP | PO</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.5 -->
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../../plugins/datatables/dataTables.bootstrap.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition skin-blue fixed sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>BDN</b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg">PT. <b>BDN</b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Buka/Tutup</span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <span class="hidden-xs">Administrator</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  Admin - Adminstrator
+                  <small>Super Profile</small>
+                </p>
+              </li>
+              
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+  </header>
+  
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MENU</li>
+        <?php do { ?>
+        <li><a href="../<?php echo $row_Menu['link']; ?>"><i class="<?php echo $row_Menu['icon']; ?>"></i> <span><?php echo $row_Menu['nama']; ?></span></a></li>
+        <?php } while ($row_Menu = mysql_fetch_assoc($Menu)); ?>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+  
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Purchase Order
+        <small>Po View </small>
+        <large><a href="InsertPOCustomer.php"><button type="button" class="btn btn-success btn-sm">New PO</button></a></large>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Purchase Order</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+
+          <div class="box">
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped table-responsive">
+                <thead>
                 <tr>
-					<td>
-						<a href="InsertPOCustomer.php"><button class="button2" type=
-							"button">Insert</button></a>
-					</td>
-				</tr>
-			</table>
-		</div>
-		<div class="ui-layout-center">
-			<table id="contentTable" class="display">
-				<thead>
-					<th >No. Reference</th>
-					<th >Tanggal</th>
-					<th >Company</th>
-					<th >Project</th>
-					<th >Amount</th>
-					<th>Opsi</th>
-				</thead>
-				<tbody>
-					<?php do { ?>
-					<tr>
-						<td ><?php echo $row_POCustomer['Reference']; ?></td>
-						<td ><?php echo $row_POCustomer['Tgl']; ?></td>
-                        <td ><?php echo $row_POCustomer['Company']; ?></td>
-						<td ><?php echo $row_POCustomer['Project']; ?></td>
-                        <td></td>
-                        <td><a href="EditPOCustomer.php?Id=<?php echo $row_POCustomer['Id']; ?>">
-                        <button type="button" class="button3">Edit PO</button></a> <a href="ViewTransaksi.php?Reference=<?php echo $row_POCustomer['Reference']; ?>">
-                        <button type="button" class="button3">View Barang</button></a></td>
-					</tr>
-					<?php } while ($row_POCustomer = mysql_fetch_assoc($POCustomer)); ?>
-				</tbody>
-			</table>
-		</div>
-	</body>
+                  <th>Reference</th>
+                  <th>Date</th>
+                  <th>Company Name</th>
+                  <th>Project</th>
+                  <th>Amount</th>
+                  <th>Opsi</th>
+                </tr>
+                </thead>
+                <tbody>
+				  <?php do { ?>
+                  <tr>
+                    <td><?php echo $row_POCustomer['Reference']; ?></td>
+                    <td><?php echo $row_POCustomer['Tgl']; ?></td>
+                    <td><?php echo $row_POCustomer['Company']; ?></td>
+                    <td><?php echo $row_POCustomer['Project']; ?></td>
+                    <td></td>
+                    <td><a href="ViewTransaksi.php?Reference=<?php echo $row_POCustomer['Reference']; ?>"><button type="button" class="btn btn-block btn-primary btn-sm">View</button></a></td>
+                  </tr>
+                  <?php } while ($row_POCustomer = mysql_fetch_assoc($POCustomer)); ?>
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>Reference</th>
+                  <th>Date</th>
+                  <th>Company Name</th>
+                  <th>Project</th>
+                  <th>Amount</th>
+                  <th>Opsi</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  <!-- Footer Wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> `1.0.0
+    </div>
+    <strong>Copyright &copy; 2015 <a href="http://apera.id">Apera Indonesia</a>.</strong> All rights
+    reserved.
+  </footer>
+  <!-- /.footer-wrapper -->
+  
+  <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+
+<!-- jQuery 2.1.4 -->
+<script src="../../plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<!-- Bootstrap 3.3.5 -->
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable();
+  });
+</script>
+</body>
 </html>
 <?php
-	mysql_free_result($POCustomer);
-
-mysql_free_result($Menu);
-	?>
+  mysql_free_result($POCustomer);
+  mysql_free_result($Menu);
+?>
---
+++ Penjualan/POCustomer/POCustomer1.php
@@ -0,0 +1,194 @@
+<?php require_once('../../Connections/Connection.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+	if (!function_exists("GetSQLValueString")) {
+	function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+	{
+	  if (PHP_VERSION < 6) {
+	    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+	  }
+	
+	  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+	
+	  switch ($theType) {
+	    case "text":
+	      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+	      break;    
+	    case "long":
+	    case "int":
+	      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+	      break;
+	    case "double":
+	      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+	      break;
+	    case "date":
+	      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+	      break;
+	    case "defined":
+	      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+	      break;
+	  }
+	  return $theValue;
+	}
+	}
+	
+mysql_select_db($database_Connection, $Connection);
+$query_POCustomer = "SELECT pocustomer.*, project.Project, customer.Company FROM pocustomer INNER JOIN project ON pocustomer.PCode=project.PCode INNER JOIN customer ON project.CCode=customer.CCode";
+$POCustomer = mysql_query($query_POCustomer, $Connection) or die(mysql_error());
+$row_POCustomer = mysql_fetch_assoc($POCustomer);
+$totalRows_POCustomer = mysql_num_rows($POCustomer);
+
+mysql_select_db($database_Connection, $Connection);
+$query_Menu = "SELECT * FROM menu";
+$Menu = mysql_query($query_Menu, $Connection) or die(mysql_error());
+$row_Menu = mysql_fetch_assoc($Menu);
+$totalRows_Menu = mysql_num_rows($Menu);
+?>
+<!------------------------------------------------------->
+<link rel="stylesheet" type="text/css" href="../../JQuery/Layout/layout.css">
+<script type="text/javascript" src="../../JQuery/Layout/jquery.js"></script>
+<script type="text/javascript" src="../../JQuery/Layout/jquery.ui.all.js"></script>
+<script type="text/javascript" src="../../JQuery/Layout/jquery.layout.js"></script>
+<script type='text/javascript'>
+	var jq126 = jQuery.noConflict();
+</script>
+<script type="text/javascript">
+	var myLayout;// a var is required because this page utilizes: myLayout.allowOverflow() method
+	
+	jq126(document).ready(function () {
+	myLayout = jq126('body').layout({
+		// enable showOverflow on west-pane so popups will overlap north pane
+		west__showOverflowOnHover: true
+	
+	//,	west__fxSettings_open: { easing: "easeOutBounce", duration: 750 }
+	});
+	
+	});
+	
+</script>
+<!------------------------------------------------------->
+<!-- DataTables CSS -->
+<link rel="stylesheet" type="text/css" href="../../JQuery/DataTable/css/jquery.dataTables.css">
+<!-- jQuery -->
+<script type="text/javascript" charset="utf8" src="../../JQuery/DataTable/js/jquery.js"></script>
+<!-- DataTables -->
+<script type="text/javascript" charset="utf8" src="../../JQuery/datatable/js/jquery.dataTables.js"></script>
+<script>
+	$(document).ready( function () {
+	    $('#contentTable').DataTable();
+	} );
+	
+</script>
+<!------------------------------------------------------->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>PT. BDN | PO Customer</title>
+		<link href="../../Button.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="ui-layout-north">
+			<div class="title">
+				PO CUSTOMER
+			</div>
+		</div>
+		<div class="ui-layout-west">
+			<table class="menuTable">
+					
+				<?php do { ?>    
+                	<tr>
+                    <td class="Menu">
+                    <a href="../<?php echo $row_Menu['link']; ?>">
+                    <button type="button" class="button">
+					<?php echo $row_Menu['nama']; ?></button></a></td>
+                    </tr>
+                    
+                <?php } while ($row_Menu = mysql_fetch_assoc($Menu)); ?>
+                    <tr>
+                    <td class="Menu">&nbsp;</td>
+                    </tr>
+                    
+			</table>
+		</div>
+		<div class="ui-layout-south">
+			<div class="footer">
+				PT. Berlian Djaya Nusantara
+			</div>
+			<div class="copyright">
+				Powered by Apera ERP
+			</div>
+		</div>
+		<div class="ui-layout-east">
+			<table class="menuTable">
+                <tr>
+					<td>
+						<a href="InsertPOCustomer.php"><button class="button2" type=
+							"button">Insert</button></a>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div class="ui-layout-center">
+			<table id="contentTable" class="display">
+				<thead>
+					<th >No. Reference</th>
+					<th >Tanggal</th>
+					<th >Company</th>
+					<th >Project</th>
+					<th >Amount</th>
+					<th>Opsi</th>
+				</thead>
+				<tbody>
+					<?php do { ?>
+					<tr>
+						<td ><?php echo $row_POCustomer['Reference']; ?></td>
+						<td ><?php echo $row_POCustomer['Tgl']; ?></td>
+                        <td ><?php echo $row_POCustomer['Company']; ?></td>
+						<td ><?php echo $row_POCustomer['Project']; ?></td>
+                        <td></td>
+                        <td><a href="EditPOCustomer.php?Id=<?php echo $row_POCustomer['Id']; ?>">
+                        <button type="button" class="button3">Edit PO</button></a> <a href="ViewTransaksi.php?Reference=<?php echo $row_POCustomer['Reference']; ?>">
+                        <button type="button" class="button3">View Barang</button></a></td>
+					</tr>
+					<?php } while ($row_POCustomer = mysql_fetch_assoc($POCustomer)); ?>
+				</tbody>
+			</table>
+		</div>
+	</body>
+</html>
+<?php
+	mysql_free_result($POCustomer);
+
+mysql_free_result($Menu);
+	?>
