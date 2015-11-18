ba879f122fb8add396f3f4fc843e972b8ea5e105
Victor Hartanto <hartantovictor27@gmail.com>
2015-11-18 13:05:52 +0700

    insert sj kembali

--- Penjualan/SJKembali/InsertSJKembali.php
+++ Penjualan/SJKembali/InsertSJKembali.php
@@ -65,36 +65,235 @@ $NoSJ = mysql_query($query_NoSJ, $Connection) or die(mysql_error());
 $row_NoSJ = mysql_fetch_assoc($NoSJ);
 $totalRows_NoSJ = mysql_num_rows($NoSJ);
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-<link href="../../Button.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-body {
-	background-image: url(../../Image/Wood.png);
-	background-repeat: no-repeat;
-}
-</style>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>BDN ERP | Insert PO</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.5 -->
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+  <!-- jQueryUI -->
+  <link rel="stylesheet" href="../../plugins/jQueryUI/jquery-ui.css" >
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../../plugins/datatables/dataTables.bootstrap.css">
+  <!-- datepicker -->
+  <link rel="stylesheet" href="../../plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
 
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition skin-blue fixed sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>BDN</b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg">PT. <b>BDN</b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Buka/Tutup</span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <span class="hidden-xs">Administrator</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  Admin - Adminstrator
+                  <small>Super Profile</small>
+                </p>
+              </li>
+              
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+  </header>
+  
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MENU</li>
+        <?php do { ?>
+        <li><a href="../<?php echo $row_Menu['link']; ?>"><i class="<?php echo $row_Menu['icon']; ?>"></i> <span><?php echo $row_Menu['nama']; ?></span></a></li>
+        <?php } while ($row_Menu = mysql_fetch_assoc($Menu)); ?>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+  
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Insert SJ Kembali
+        <small>New</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="customer.php">SJ Kembali</a></li>
+        <li class="active">Insert SJ Kembali</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <!-- general form elements -->
+            <div class="box box-primary">
+              <div class="box-header with-border">
+                <h3 class="box-title">SJ Detail</h3>
+              </div>
+              <!-- /.box-header -->
+              <!-- form start -->
+              <form action="<?php echo $editFormAction; ?>" id="form1" name="form1" method="POST">
+                <div class="box-body">
+                  <div class="form-group">
+                    <label>No. Surat Jalan</label>
+                    <input name="SJKem" type="text" class="form-control" id="SJKem" onKeyUp="capital()" value="<?php echo str_pad($row_NoSJ['Id']+1, 3, "0", STR_PAD_LEFT); ?>/SI/<?php echo date("mY") ?>" readonly>
+                  </div>
+                  <div class="form-group">
+                    <label>Send Date</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                      <input name="Tgl" type="text" autocomplete="off" class="form-control pull-right date" id="Tgl">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label>PO Code</label>
+                    <input name="Reference" type="text" class="form-control" id="Reference" autocomplete="off" onKeyUp="capital()" placeholder="ABC01">
+                    <p class="help-block">Enter the beginning of the PO Code, then pick from the dropdown</p>
+                  </div>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                  <a href="SJKembali.php"><button type="button" class="btn btn-default pull-left">Back</button></a> 
+                  <button type="submit" id="submit" class="btn btn-primary pull-right">Insert</button>
+                </div>
+              <input type="hidden" name="MM_insert" value="form1">
+              </form>
+            </div>
+            <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  <!-- Footer Wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> `1.0.0
+    </div>
+    <strong>Copyright &copy; 2015 <a href="http://apera.id">Apera Indonesia</a>.</strong> All rights
+    reserved.
+  </footer>
+  <!-- /.footer-wrapper -->
+  
+  <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+
+<!-- jQuery 2.1.4 -->
+<script src="../../	plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<!-- jQuery UI -->
+<script src="../../plugins/jQueryUI/jquery-ui.js"></script>
+<!-- Bootstrap 3.3.5 -->
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<!-- datepicker -->
+<script src="../../plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false
+    });
+  });
+</script>
 <script>
 function capital() {
-	var x = document.getElementById("Reference");
+    var x = document.getElementById("CCode");
+    x.value = x.value.toUpperCase();
+	var x = document.getElementById("Company");
     x.value = x.value.toUpperCase();
 }
 </script>
-
-<link href="/scaffolding/JQuery2/jquery-ui.css" rel="stylesheet" type="text/css">
-<script src="../../JQuery2/external/jquery/jquery.js"></script>
-<script src="../../JQuery2/jquery-ui.js"></script>
-
 <script>
-$(function() {
-  $( "#Tgl" ).datepicker();
-});
+  $('#Tgl').datepicker({
+	  format: "dd/mm/yyyy",
+	  orientation: "bottom left",
+	  todayHighlight: true,
+	  autoclose: true
+  }); 
 </script>
-
 <script type="text/javascript">
 $(function() {
     var availableTags = <?php include ("../autocomplete3.php");?>;
@@ -103,91 +302,12 @@ $(function() {
     });
   });
 </script>
-
-</head>
-
-<body>
-<div style="float:left;width:15%">
-  <table width="200" border="0">
-    <tbody>
-      <tr>
-        <th>&nbsp;</th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-      </tr>
-					
-				<?php do { ?>    
-                	<tr>
-                    <td class="Menu">
-                    <a href="../<?php echo $row_Menu['link']; ?>">
-                    <button type="button" class="button">
-					<?php echo $row_Menu['nama']; ?></button></a></td>
-                    </tr>
-                    
-                <?php } while ($row_Menu = mysql_fetch_assoc($Menu)); ?>
-                    <tr>
-                    <td class="Menu">&nbsp;</td>
-                    </tr>
-                    
-    </tbody>
-  </table>
-</div>
-
-<div style="float:left;width:85%">
-
-<table width="800" border="0">
-
-
-  <tbody>
-    <tr>
-      <th align="center"><h2>INSERT</h2></th>
-    </tr>
-  </tbody>
-</table>
-</p>
-<form action="<?php echo $editFormAction; ?>" id="form1" name="form1" method="POST">
-  <table width="1000" border="0">
-    <tbody>
-      <tr>
-        <th width="250">&nbsp;</th>
-        <th width="125" align="right">No. SJ</th>
-        <th width="75" align="right">&nbsp;</th>
-        <td width="532"><input name="SJKem" type="text" class="textbox" id="SJKem" value="<?php echo str_pad($row_NoSJ['Id']+1, 3, "0", STR_PAD_LEFT); ?>/SI/<?php echo date("mY") ?>" readonly></td>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-        <th align="right">Tanggal Tertanda</th>
-        <th align="right">&nbsp;</th>
-        <td><input name="Tgl" type="text" id="Tgl" autocomplete="off" class="textbox"></td>
-      </tr>
-      <tr>
-        <th>&nbsp;</th>
-        <th align="right">No. Reference</th>
-        <th align="right">&nbsp;</th>
-        <td><input name="Reference" type="text" id="Reference" autocomplete="off" onKeyUp="capital()" class="textbox"></td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td align="center">&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td colspan="2" align="center"><input type="submit" name="submit" id="submit" class="submit" value="Insert"></td>
-        <td><a href="SJKembali.php"><button type="button" class="submit">Cancel</button></a></td>
-      </tr>
-    </tbody>
-  </table>
-  <input type="hidden" name="MM_insert" value="form1">
-</form>
+<script>
+function capital() {
+	var x = document.getElementById("PCode");
+    x.value = x.value.toUpperCase();
+}
+</script>
 </body>
 </html>
 <?php
