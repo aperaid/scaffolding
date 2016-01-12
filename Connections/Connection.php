bf416d56256dcc7e05ce2ecf9a11a583035e0a09
Victor Hartanto <hartantovictor27@gmail.com>
2016-01-12 12:12:23 +0700

    Connection update

--- Connections/Connection.php
+++ Connections/Connection.php
@@ -5,9 +5,9 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_Connection = "localhost";
-$database_Connection = "scaffoldingjual";
-$username_Connection = "root";
-$password_Connection = "";
+$hostname_Connection = "181.224.157.45";
+$database_Connection = "apera145_scaffolding";
+$username_Connection = "apera145_bdn";
+$password_Connection = "890iop890iop";
 $Connection = mysql_pconnect($hostname_Connection, $username_Connection, $password_Connection) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
