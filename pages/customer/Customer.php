816a9e7e04f85c11af39e5d49118b31ea0183659
Victor Hartanto <hartantovictor27@gmail.com>
2016-01-13 16:21:45 +0700

    customer update

--- pages/customer/Customer.php
+++ pages/customer/Customer.php
@@ -1,129 +1,126 @@
-<?php require_once('../../connection/connection.php'); ?>
+<!--PHP-->
+
+<?php
+require_once('../../connection/connection.php');
+?>
 <?php
 //initialize the session
 if (!isset($_SESSION)) {
   session_start();
 }
-
 // ** Logout the current user. **
-$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
-if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
-  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
+$logoutAction = $_SERVER['PHP_SELF'] . "?doLogout=true";
+if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")) {
+  $logoutAction .= "&" . htmlentities($_SERVER['QUERY_STRING']);
 }
-
-if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
+if ((isset($_GET['doLogout'])) && ($_GET['doLogout'] == "true")) {
   //to fully log out a visitor we need to clear the session varialbles
-  $_SESSION['MM_Username'] = NULL;
+  $_SESSION['MM_Username']  = NULL;
   $_SESSION['MM_UserGroup'] = NULL;
-  $_SESSION['PrevUrl'] = NULL;
+  $_SESSION['PrevUrl']      = NULL;
   unset($_SESSION['MM_Username']);
   unset($_SESSION['MM_UserGroup']);
   unset($_SESSION['PrevUrl']);
-	
-  $logoutGoTo = "../../Users/Login.php";
+  $logoutGoTo = "../../pages/login/Login.php";
   if ($logoutGoTo) {
     header("Location: $logoutGoTo");
     exit;
   }
 }
 ?>
+
 <?php
 if (!isset($_SESSION)) {
   session_start();
 }
-$MM_authorizedUsers = "Admin";
+$MM_authorizedUsers  = "Admin";
 $MM_donotCheckaccess = "true";
-
 // *** Restrict Access To Page: Grant or deny access to this page
-function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup)
+{
   // For security, start by assuming the visitor is NOT authorized. 
-  $isValid = False; 
-
+  $isValid = False;
   // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
   // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
-  if (!empty($UserName)) { 
+  if (!empty($UserName)) {
     // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
     // Parse the strings into arrays. 
-    $arrUsers = Explode(",", $strUsers); 
-    $arrGroups = Explode(",", $strGroups); 
-    if (in_array($UserName, $arrUsers)) { 
-      $isValid = true; 
-    } 
+    $arrUsers  = Explode(",", $strUsers);
+    $arrGroups = Explode(",", $strGroups);
+    if (in_array($UserName, $arrUsers)) {
+      $isValid = true;
+    }
     // Or, you may restrict access to only certain users based on their username. 
-    if (in_array($UserGroup, $arrGroups)) { 
-      $isValid = true; 
-    } 
-    if (($strUsers == "") && true) { 
-      $isValid = true; 
-    } 
-  } 
-  return $isValid; 
+    if (in_array($UserGroup, $arrGroups)) {
+      $isValid = true;
+    }
+    if (($strUsers == "") && true) {
+      $isValid = true;
+    }
+  }
+  return $isValid;
 }
-
 $MM_restrictGoTo = "../../Users/Login.php";
-if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
-  $MM_qsChar = "?";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("", $MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {
+  $MM_qsChar   = "?";
   $MM_referrer = $_SERVER['PHP_SELF'];
-  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
-  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
-  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
-  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
-  header("Location: ". $MM_restrictGoTo); 
+  if (strpos($MM_restrictGoTo, "?"))
+    $MM_qsChar = "&";
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0)
+    $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+  $MM_restrictGoTo = $MM_restrictGoTo . $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: " . $MM_restrictGoTo);
   exit;
 }
 ?>
+
 <?php
 if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
+  {
+    if (PHP_VERSION < 6) {
+      $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+    }
+    $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+    switch ($theType) {
+      case "text":
+        $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+        break;
+      case "long":
+      case "int":
+        $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+        break;
+      case "double":
+        $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+        break;
+      case "date":
+        $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+        break;
+      case "defined":
+        $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+        break;
+    }
+    return $theValue;
   }
-
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
 }
-}
-
 mysql_select_db($database_Connection, $Connection);
 $query_Customer = "SELECT * FROM customer ORDER BY Id ASC";
 $Customer = mysql_query($query_Customer, $Connection) or die(mysql_error());
-$row_Customer = mysql_fetch_assoc($Customer);
+$row_Customer       = mysql_fetch_assoc($Customer);
 $totalRows_Customer = mysql_num_rows($Customer);
-
 mysql_select_db($database_Connection, $Connection);
 $query_Menu = "SELECT * FROM menu";
 $Menu = mysql_query($query_Menu, $Connection) or die(mysql_error());
-$row_Menu = mysql_fetch_assoc($Menu);
+$row_Menu       = mysql_fetch_assoc($Menu);
 $totalRows_Menu = mysql_num_rows($Menu);
-
-$colname_User = "-1";
+$colname_User   = "-1";
 if (isset($_SESSION['MM_Username'])) {
   $colname_User = $_SESSION['MM_Username'];
 }
 mysql_select_db($database_Connection, $Connection);
 $query_User = sprintf("SELECT Name FROM users WHERE Username = %s", GetSQLValueString($colname_User, "text"));
 $User = mysql_query($query_User, $Connection) or die(mysql_error());
-$row_User = mysql_fetch_assoc($User);
+$row_User       = mysql_fetch_assoc($User);
 $totalRows_User = mysql_num_rows($User);
 ?>
 
@@ -176,7 +173,6 @@ $totalRows_User = mysql_num_rows($User);
 
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -187,7 +183,6 @@ $totalRows_User = mysql_num_rows($User);
               <!-- User image -->
               <li class="user-header">
                 <img src="../../library/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
                 <p>
                   <?php echo $_SESSION['MM_Username']; ?> - <?php echo $row_User['Name']; ?>
                   <small>Super Profile</small>
@@ -202,7 +197,6 @@ $totalRows_User = mysql_num_rows($User);
               </li>
             </ul>
           </li>
-          
         </ul>
       </div>
     </nav>
@@ -216,7 +210,7 @@ $totalRows_User = mysql_num_rows($User);
       <ul class="sidebar-menu">
         <li class="header">MENU</li>
         <?php do { ?>
-        <li><a href="../<?php echo $row_Menu['link']; ?>"><i class="<?php echo $row_Menu['icon']; ?>"></i> <span><?php echo $row_Menu['nama']; ?></span></a></li>
+        <li><a href="../../<?php echo $row_Menu['link']; ?>"><i class="<?php echo $row_Menu['icon']; ?>"></i> <span><?php echo $row_Menu['nama']; ?></span></a></li>
         <?php } while ($row_Menu = mysql_fetch_assoc($Menu)); ?>
       </ul>
     </section>
@@ -230,7 +224,7 @@ $totalRows_User = mysql_num_rows($User);
       <h1>
         Customer
         <small>Customer View </small>
-        <large><a href="InsertCustomer.php"><button type="button" class="btn btn-success btn-sm">New Customer</button></a></large>
+        <large><a href="InsertCustomer.php"><button id="bt_customer_insert" name="bt_customer_insert" type="button" class="btn btn-success btn-sm">New Customer</button></a></large>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -245,7 +239,7 @@ $totalRows_User = mysql_num_rows($User);
 
           <div class="box">
             <div class="box-body">
-              <table id="example1" class="table table-condensed table-hover">
+              <table id="tb_customer" name="tb_customer" class="table table-condensed table-hover">
                 <thead>
                 <tr>
                   <th>Customer Code</th>
@@ -262,12 +256,6 @@ $totalRows_User = mysql_num_rows($User);
                   </tr>
                   <?php } while ($row_Customer = mysql_fetch_assoc($Customer)); ?>
                 </tbody>
-                <tfoot>
-                <tr>
-                  <th>Customer Code</th>
-                  <th>Company Name</th>
-                </tr>
-                </tfoot>
               </table>
             </div>
             <!-- /.box-body -->
@@ -314,13 +302,12 @@ $totalRows_User = mysql_num_rows($User);
 <!-- page script -->
 <script>
   $(function () {
-    $("#example1").DataTable();
+    $("tb_customer").DataTable();
   });
 </script>
 </body>
 </html>
 <?php
 mysql_free_result($Customer);
-
 mysql_free_result($User);
 ?>
