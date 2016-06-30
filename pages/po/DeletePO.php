1b09d39808e4987106456640d1108c9e3375c2f9
Victor Hartanto <hartantovictor27@gmail.com>
2016-06-30 15:16:56 +0700

    alter table

--- pages/po/DeletePO.php
+++ pages/po/DeletePO.php
@@ -28,6 +28,16 @@ if ((isset($_GET['POCode'])) && ($_GET['POCode'] != "")) {
 	mysql_free_result($deletepotransaksi);
 	//2. Delete PO in transaksi
 	
+	//3. Alter PO 
+	$query_alterpo = sprintf("ALTER TABLE po AUTO_INCREMENT = 1");
+	$alterpo = mysql_query($query_alterpo, $Connection) or die(mysql_error());
+	mysql_free_result($alterpo);
+	
+	//4. Alter transaksi
+	$query_altertransaksi = sprintf("ALTER TABLE po AUTO_INCREMENT = 1");
+	$altertransaksi = mysql_query($query_altertransaksi, $Connection) or die(mysql_error());
+	mysql_free_result($altertransaksi);
+	
 	$deleteGoTo = "../pocustomer/ViewTransaksi.php?Reference=$ref";
 	if (isset($_SERVER['QUERY_STRING'])) {
 		$deleteGoTo .= (strpos($deleteGoTo, '?')) ? "&" : "?";
