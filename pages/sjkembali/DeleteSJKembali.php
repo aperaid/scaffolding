7d019474ebfb6cdb44e36a0d86320e5d5b02d97d
Victor Hartanto <hartantovictor27@gmail.com>
2016-07-01 19:06:32 +0700

    sjkembali login system

--- pages/sjkembali/DeleteSJKembali.php
+++ pages/sjkembali/DeleteSJKembali.php
@@ -1,37 +1,10 @@
 <?php require_once('../../connections/Connection.php'); ?>
 <?php
-if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
+// Declare Root directory
+$ROOT="../../";
 
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
-}
-
-
+include($ROOT . "pages/login/session.php");
+include_once($ROOT . "pages/functionphp.php");
 
 // Below old code
 mysql_select_db($database_Connection, $Connection);
