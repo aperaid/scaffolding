a52c68dfeb6342b3aa0ce9592e370bd5c001182c
Victor Hartanto <hartantovictor27@gmail.com>
2016-07-01 18:45:27 +0700

    login system

--- pages/transaksisewa/ExtendTransaksiSewa.php
+++ pages/transaksisewa/ExtendTransaksiSewa.php
@@ -1,36 +1,11 @@
 <?php require_once('../../connections/Connection.php'); ?>
 <?php date_default_timezone_set("Asia/Krasnoyarsk"); ?>
 <?php
-if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
+// Declare Root directory
+$ROOT="../../";
 
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
-}
+include($ROOT . "pages/login/session.php");
+include_once($ROOT . "pages/functionphp.php");
 
 $editFormAction = $_SERVER['PHP_SELF'];
 if (isset($_SERVER['QUERY_STRING'])) {
