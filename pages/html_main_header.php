e64d892dc7ceaad36d861bbf6df4be65167dfe36
Victor Hartanto <hartantovictor27@gmail.com>
2016-07-04 12:18:26 +0700

    main header

--- pages/html_main_header.php
+++ pages/html_main_header.php
@@ -15,14 +15,14 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src= "<?php echo $ROOT ?>library/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs"><?php echo "Welcome ".$_SESSION['MM_Username']; ?></span>
+              <span class="hidden-xs"><?php echo "Welcome " . $_SESSION['username']; ?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
                 <img src="<?php echo $ROOT ?>library/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                 <p>
-                  <?php echo $_SESSION['MM_Username']; ?> - <?php echo $row_User['Name']; ?>
+                  <?php echo $_SESSION['username']; ?> - <?php echo $row_User['Name']; ?>
                   <small>Super Profile</small>
                 </p>
               </li>
