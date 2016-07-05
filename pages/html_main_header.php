9c3051a743666cc1eeca992e99672bee723880ae
Victor Hartanto <hartantovictor27@gmail.com>
2016-07-05 16:05:20 +0700

    html main header

--- pages/html_main_header.php
+++ pages/html_main_header.php
@@ -1,43 +1,32 @@
-<header class="main-header">
-    <!-- Logo -->
-    <?php include_once($ROOT . 'pages/logo.php'); ?>
-    
+	<header class="main-header">
+	<!-- Logo -->
+	<?php include_once($ROOT . 'pages/logo.php'); ?>
+      
 	<!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top" role="navigation">
-      <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-        <span class="sr-only">Buka/Tutup</span>
-      </a>
-
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <!-- User Account: style can be found in dropdown.less -->
-          <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src= "<?php echo $ROOT ?>library/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs"><?php echo "Welcome " . $_SESSION['username']; ?></span>
-            </a>
-            <ul class="dropdown-menu">
-              <!-- User image -->
-              <li class="user-header">
-                <img src="<?php echo $ROOT ?>library/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                <p>
-                  <?php echo $_SESSION['username']; ?> - <?php echo $row_User['Name']; ?>
-                  <small>Super Profile</small>
-                </p>
-              </li>
-              
-              <!-- Menu Footer-->
-              <li class="user-footer">
-                <div class="pull-right">
-                  <a href="<?php echo $logoutAction ?>" class="btn btn-default btn-flat">Sign out</a>
-                </div>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
+	<nav class="navbar navbar-static-top" role="navigation">
+		<!-- Sidebar toggle button-->
+		<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+			<span class="sr-only">Buka/Tutup</span>
+		</a>
+		<div class="navbar-custom-menu">
+			<ul class="nav navbar-nav">
+				<!-- User Account: style can be found in dropdown.less -->
+				<li class="dropdown user user-menu">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<span class="hidden-xs"><?php echo $_SESSION['username']; ?></span>
+					</a>			
+					<ul class="dropdown-menu">
+						<!-- Menu Footer-->
+						<li class="user-footer">
+							<div class="pull-right">
+								<a href="<?php echo $logoutAction ?>" class="btn btn-default btn-flat">Sign out</a>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</nav>
 </header>
 <!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
